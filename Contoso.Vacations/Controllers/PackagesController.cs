7abf5e16c49e8304209af00042df2d55db65b3fa
Umar Mohamed Usman <umarm@microsoft.com>
2020-01-28 01:24:52 -0800

    Initial Version

---
+++ Contoso.Vacations/Controllers/PackagesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contoso.Vacations.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Contoso.Vacations.Controllers
+{
+    [Route("api/[controller]")]
+    public class PackagesController : Controller
+    {
+
+        private List<Package> _packages = new List<Package>
+         {
+                    new Package { PackageId = 1, PackageName = "Maui", NumberOfNights=7, Price=3008},
+                    new Package { PackageId = 2, PackageName = "Honolulu", NumberOfNights = 5, Price = 2005 },
+                    new Package { PackageId = 3, PackageName = "Big Island", NumberOfNights = 6, Price = 2500 },
+                    new Package { PackageId = 3, PackageName = "Miami Beach", NumberOfNights = 7, Price = 2800 }
+         };
+
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<Package> Get()
+        {
+            return _packages;
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public Package Get(int id)
+        {
+            return _packages.Find(x => x.PackageId == id);
+        }
+
+
+
+    }
+}
---
+++ Contoso.Vacations/Models/Package.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contoso.Vacations.Models
+{
+    public class Package
+    {
+        public int PackageId { get; set; }
+
+        public string PackageName { get; set; }
+
+        public int NumberOfNights { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
